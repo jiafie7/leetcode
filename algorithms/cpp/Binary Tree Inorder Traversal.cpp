c2d944b9b1fde334a61d92c9db2ade0d172c25bc
Jiafie <67643874+jiafie7@users.noreply.github.com>
2021-04-17 00:50:13 +0800

    Create Binary Tree Inorder Traversal.cpp

---
+++ algorithms/cpp/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,94 @@
+// Source : https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+// Author : Hang Zhang
+// Date   : 2021-04-17
+
+/**********************************************************************************************************
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Example 1:
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * Example 2:
+ * Input: root = []
+ * Output: []
+ * 
+ * Example 3:
+ * Input: root = [1]
+ * Output: [1]
+ *
+ * Example 4:
+ * Input: root = [1,2]
+ * Output: [2,1]
+ * 
+ * Example 5:
+ * Input: root = [1,null,2]
+ * Output: [1,2]
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ **********************************************************************************************************/
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Method 1: Recursive
+
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& res) 
+    {
+        if (!root) return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
+
+// Method 2: Iteration
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk; 
+
+        while (root != nullptr || !stk.empty()) { //
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            res.push_back(root->val);
+          
+            root = root->right;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
