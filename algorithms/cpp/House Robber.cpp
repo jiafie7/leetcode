1a8b93fc6952119eb826637bfff13e47981b282b
Jiafie <67643874+jiafie7@users.noreply.github.com>
2021-04-18 23:52:03 +0800

    Create House Robber.cpp

---
+++ algorithms/cpp/House Robber.cpp
@@ -0,0 +1,81 @@
+// Source : https://leetcode-cn.com/problems/house-robber/
+// Author : Hang Zhang
+// Date   : 2021-04-18
+
+/**********************************************************************************************************
+ *
+ * You are a professional robber planning to rob houses along a street.
+ * Each house has a certain amount of money stashed, the only constraint stopping you from robbing 
+ * each of them is that adjacent houses have security systems connected and it will automatically contact 
+ * the police if two adjacent houses were broken into on the same night.
+ *
+ * Given an integer array nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ *
+ * Example 2:
+ * Input: nums = [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ *
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ * 
+ **********************************************************************************************************/
+
+// Method 1:
+
+// dp[i]=max(dp[i−2]+nums[i],dp[i−1]).
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        //dp[i] is used to store the maximum amount of money stolen from the former i houses.
+        vector<int> dp = vector<int>(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++ i) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+};
+
+// Method 2:
+
+// The maximum total amount for each house is only related to the maximum total amount for the first two houses in that house,
+// so at each moment it is necessary to store only the highest total amount of the first two houses.
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        int first = nums[0], second = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++ i) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+};
+
+
+
+
