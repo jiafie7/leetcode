53262e9d8dfb7828ae2a935494c776058e1836ff
Jiafie <67643874+jiafie7@users.noreply.github.com>
2022-01-05 10:13:53 +0800

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ algorithms/cpp/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,34 @@
+// Source : https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+// Author : Hang Zhang 
+// Date   : 2022-01-05
+
+/*****************************************************************************************************************
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
+ ****************************************************************************************************************/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<int, int> heap;
+        int res = 0;
+        for (int i = 0, j = 0; i < s.size(); i ++ ) {
+            heap[s[i]] ++ ;
+            while (heap[s[i]] > 1) heap[s[j ++ ]] -- ;
+            res = max(res, i - j + 1);
+        } 
+        return res;
+    }
+};
+
