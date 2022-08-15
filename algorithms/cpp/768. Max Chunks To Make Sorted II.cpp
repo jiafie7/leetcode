6862c8d33d1207541710668f6ad52c146745d259
Jiafie <67643874+jiafie7@users.noreply.github.com>
2022-08-15 21:07:24 +0800

    Update 768. Max Chunks To Make Sorted II.cpp

--- algorithms/cpp/768. Max Chunks To Make Sorted II.cpp
+++ algorithms/cpp/768. Max Chunks To Make Sorted II.cpp
@@ -11,9 +11,7 @@
  *
  * Example 1:
  * Input: arr = [5,4,3,2,1]
- *
  * Output: 1
- *
  * Explanation:
  * Splitting into two or more chunks will not return the required result.
  * For example, splitting into [5, 4], [3, 2, 1] will result in [4, 5, 1, 2, 3], which isn't sorted.
