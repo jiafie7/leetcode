d8d3fa2db860d47b6e65efa82bdb7f63c52388f2
Jiafie <67643874+jiafie7@users.noreply.github.com>
2022-08-22 21:09:16 +0800

    Create 654. Maximum Binary Tree.java

---
+++ algorithms/Java/654. Maximum Binary Tree.java
@@ -0,0 +1,60 @@
+// Source : https://leetcode.cn/problems/maximum-binary-tree/
+// Author : Hang Zhang
+// Date   : 2022-08-22
+
+/*************************************************************************************************************************
+ *
+ * Create a root node whose value is the maximum value in nums.
+ * Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+ * Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+ * Return the maximum binary tree built from nums.
+ * 
+ * Example 1:
+ * Input: nums = [3,2,1,6,0,5]
+ * Output: [6,3,5,null,2,0,null,null,1]
+ * Explanation: The recursive calls are as follow:
+ * - The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right suffix is [0,5].
+ *     - The largest value in [3,2,1] is 3. Left prefix is [] and right suffix is [2,1].
+ *         - Empty array, so no child.
+ *         - The largest value in [2,1] is 2. Left prefix is [] and right suffix is [1].
+ *             - Empty array, so no child.
+ *             - Only one element, so child is a node with value 1.
+ *     - The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
+ *         - Only one element, so child is a node with value 0.
+ *         - Empty array, so no child.
+ *
+ **************************************************************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Stack<TreeNode> stk = new Stack<>();
+        for (int x: nums) {
+            TreeNode node = new TreeNode(x);
+            while (!stk.empty() && stk.peek().val < x) {
+                node.left = stk.peek();
+                stk.pop();
+            }
+
+            if (!stk.empty()) stk.peek().right = node;
+            stk.push(node);
+        }
+
+        while (stk.size() > 1) stk.pop();
+        return stk.peek();
+    }
+}
