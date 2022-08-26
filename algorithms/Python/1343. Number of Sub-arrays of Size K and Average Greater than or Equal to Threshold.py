7b77e5ec76b79ca062f11b19ba77248eaef22424
Jiafie <67643874+jiafie7@users.noreply.github.com>
2022-08-26 21:10:36 +0800

    Create 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py

---
+++ algorithms/Python/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
@@ -0,0 +1,30 @@
+// Source : https://leetcode.cn/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/
+// Author : Hang Zhang
+// Date   : 2022-08-26
+
+/************************************************************************************************************************
+ *
+ * Given an array of integers arr and two integers k and threshold,
+ * return the number of sub-arrays of size k and average greater than or equal to threshold.
+ *
+ * Example 1:
+ * Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+ * Output: 3
+ * Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively.
+ * All other sub-arrays of size 3 have averages less than 4 (the threshold).
+ *
+ ***********************************************************************************************************************/
+ 
+ class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        res, sum, i, j = 0, 0, 0, 0
+        while i < len(arr):
+            sum += arr[i]
+            if i - j >= k:
+                sum -= arr[j]
+                j += 1
+            if i - j + 1 == k and sum >= k * threshold:
+                res += 1
+            i += 1
+        return res
+ 
