99b861875cceec1b12638f8fa96b9424bb4c7b2b
Jiafie <67643874+jiafie7@users.noreply.github.com>
2022-08-27 20:36:18 +0800

    Create 1344. Angle Between Hands of a Clock.py

---
+++ algorithms/Python/1344. Angle Between Hands of a Clock.py
@@ -0,0 +1,22 @@
+// Source : https://leetcode.cn/problems/angle-between-hands-of-a-clock/
+// Author : Hang Zhang
+// Date   : 2022-08-27
+
+/************************************************************************************************************************
+ *
+ * Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
+ * Answers within 10-5 of the actual value will be accepted as correct.
+ * 
+ * Example 1:
+ * Input: hour = 12, minutes = 30
+ * Output: 165
+ *
+ ************************************************************************************************************************/
+  
+  class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        hour %= 12
+        x = minutes * 6
+        y = minutes * 0.5 + hour * 30
+        return min(abs(x - y), 360 - abs(x - y))
+  
