21275eea066c44ea732548856739ba7fa1006a39
jiafie7 <jiafie7@gmail.com>
2024-03-17 15:22:29 +0800

    commit 3Sum

---
+++ algorithms/cpp/3Sum/3Sum.cpp
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/3sum/description/
+// Author : Garfield Zhang
+// Date   : 2024-03-17
+
+/****************************************************************************************
+ *
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ ****************************************************************************************/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i ++ ) {
+            if (i && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1, k = nums.size() - 1; j < k; j ++ ) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                while (j < k - 1 && nums[i] + nums[j] + nums[k - 1] >= 0) k -- ;
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                }
+            }
+        }
+        return res;
+    }
+};
