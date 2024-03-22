37a9d51015ae8ab0262dce47853cffc97ab11450
jiafie7 <jiafie7@gmail.com>
2024-03-22 08:34:14 +0800

    commit mergeTwoSortedLists

---
+++ algorithms/cpp/mergeTwoSortedLists/mergeTwoSortedLists.cpp
@@ -0,0 +1,45 @@
+// Source : https://leetcode.com/problems/merge-two-sorted-lists/
+// Author : Garfield Zhang
+// Date   : 2024-03-22
+
+/****************************************************************************************
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists into one sorted list. The list should be made by splicing 
+ * together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ ****************************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *cur = dummy;
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                cur = cur->next = list1;
+                list1 = list1->next;
+            } else {
+                cur = cur->next = list2;
+                list2 = list2->next;
+            }
+        }
+        if (list1) cur->next = list1;
+        if (list2) cur->next = list2;
+        
+        return dummy->next;
+    }
+};
